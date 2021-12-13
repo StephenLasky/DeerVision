65608d349961a915638c89ebfe7e16350a520bb9
StephenLasky <StephenJLasky@gmail.com>
2021-12-12 19:41:36 -0600

    Adding files in folder function.

--- common.py
+++ common.py
@@ -1,4 +1,5 @@
 import cv2
+import os
 
 def num_frames(cap):
     return int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
@@ -7,3 +8,14 @@ def get_frame(cap, num):
     cap.set(1, num)
     success, frame = cap.read()
     return frame
+
+def get_files_in_folder(folder):
+    contents = os.listdir(folder)
+    return_contents = []
+
+    for content in contents:
+        if os.path.isfile(folder + content): return_contents.append(folder + content)
+        elif os.path.isdir(folder + content): return_contents += get_files_in_folder(folder + content + "/")
+        else: print(folder + content + " is not a file/dir")
+
+    return return_contents
